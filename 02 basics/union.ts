cfd5274d9187aa7bc5caa9e0542df342c5ac85e2
itssinghankit <singhankit.kr@gmail.com>
2024-06-27 18:40:24 +0530

    union,union of Type, typeof, limited set of values

---
+++ 02 basics/union.ts
@@ -0,0 +1,43 @@
+let score:number | string = 33
+score="sfsf"
+
+//union of Type
+type Admin ={
+    username:string
+}
+let unionType:Admin| string={username:"Ankit"}
+
+//Type Guard
+//typeof
+
+function getDbId(id:number | string){
+    console.log(`DB id is ${id}`)
+}
+getDbId(33)
+getDbId("33")
+
+// function getDbId2(id:number | string){
+//    id.toUpperCase() //error because of number
+// }
+
+function getDbId3(id:number | string){
+ if(typeof id ==="string"){
+    id.toUpperCase()
+ }
+}
+
+//array of different types using union
+const data:(string|number)[]=[1,2,"3","4"]
+
+// error - either string or array of number
+// const data2: string | number[]=[1,2,"3"] 
+
+//either array of string or array of number
+const data2: string[] |  number[]=["1","2"]
+
+let person : "Ankit" | "pushkar"
+person="pushkar"
+// person="rahul" //error
+
+
+
--- Readme.md
+++ Readme.md
@@ -1,11 +1,14 @@
 ## Typescript tutorial
 
 # Table of contents-
-* Introduction to typescript
-* Variables
-* Types Specification
-* More about Any,void,never keyword
-* object, object as return type and parameter, object passing through function call
-* type alias,type of types ("&")
-* readonly, nullables-?
-* array, array of defined Type
+
+- Introduction to typescript
+- Variables
+- Types Specification
+- More about Any,void,never keyword
+- object, object as return type and parameter, object passing through function call
+- type alias,type of types ("&")
+- readonly, nullables-?
+- array, array of defined Type,array of array of number
+- union,union of Type, typeof, limited set of values
+- 
