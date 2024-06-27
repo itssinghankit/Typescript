6842fbb34873fc34f3a5c29bfb9196c978f5bc38
itssinghankit <singhankit.kr@gmail.com>
2024-06-27 15:57:01 +0530

    when to define type explicitly

--- 01 intro/variables.ts
+++ 01 intro/variables.ts
@@ -22,6 +22,22 @@ let userId: string;
 //dont write type if you are assigning a value
 let userId2 = "ankit";
 
-let num=10;
-num=20;
+let num = 10;
+num = 20;
 
+//in this case automatic inference is not running so we need to specify the type
+// let hero;
+
+let hero: string;
+
+function gethero() {
+  return "hero";
+}
+
+//here type is Any if dont specify type
+// hero=gethero()
+
+hero = gethero();
+
+/* Any is not a type that you should use in your code. It is a type that TypeScript uses to represent any JavaScript value.
+ */
